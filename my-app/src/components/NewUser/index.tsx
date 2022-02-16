a66fcfe626d0a55af460adbe0947fd2cae8b3677
RolandoDrRobot <rarguello@ltvco.com>
2022-02-15 20:29:54 -0600

    ya funciona el create user

--- my-app/src/components/NewUser/index.tsx
+++ my-app/src/components/NewUser/index.tsx
@@ -1,17 +1,52 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import axios from 'axios';
+import { globalContext } from '../../hooks/appContext';
+import { Link, useNavigate } from 'react-router-dom';
 import './main.css';
 
 function NewUser() {
+  const navigate = useNavigate();
+  const { 
+    setLoginStatus,
+    setEmail } = React.useContext(globalContext);
+  let [createUserStatus, setCreateUserStatus] = React.useState<string>('When you create an account, you agree to complain with our TOS');
+
+  const CreateUser = async (email: string, password: string, name: string) => {
+    await axios.post('http://localhost:443/createUser', { email: email, password: password, name: name }).then((response) => {
+      setCreateUserStatus(response.data.status);
+      if (response.data.status === 'created') {
+        setLoginStatus('granted');
+        setEmail(email);
+        navigate('/', { replace: true });
+      }
+    });
+  }
+
+  const onSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      email: { value: string };
+      password: { value: string };
+      name: { value: string };
+    };
+    const email = target.email.value;
+    const password = target.password.value;
+    const name = target.name.value;        
+    
+    CreateUser(email, password, name);
+  }
+
   return (
     <>
       <div className="new-user d-flex align-items-center">
         <div className="container d-flex justify-content-center">
-          <form className="new-user-form" id="new-user-form">
-            <input placeholder="Email Address" type="email" />
-            <input placeholder="New Password" type="password" />
-            <input placeholder="What's your name?" type="text" />
-            <button className="main-button">Create User</button>
-            <Link to="/" className="text-button gray text-center d-block">Go back</Link>
+          <form onSubmit={onSubmitForm}>
+            <input placeholder="Email Address" name="email" type="email" />
+            <input placeholder="New Password" name="password" type="password" />
+            <input placeholder="What's your name?" name="name" type="text" />
+            <button type="submit" className="main-button">Create User</button>
+            <Link to="/" className="text-button gray text-center d-block mb-4">Go back</Link>
+            <span className="alert-text">{createUserStatus}</span>
           </form>
         </div>
       </div>
